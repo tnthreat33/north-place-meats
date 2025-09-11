4b1a97add986d460d93db771a84827176cd2cf71
rhamb <rhamb@RACHELYOGA>
2025-09-10 20:01:57 -0400

    add faq

--- src/pages/Faqs.js
+++ src/pages/Faqs.js
@@ -1,4 +1,39 @@
-// src/pages/Home.js
+import "../App.css";
+
 export default function Faqs() {
-  return <h1>Welcome to FAQ</h1>;
+  return (
+    <div className="faq-container">
+      <div className="faq-left">
+        <h1>Frequently</h1>
+        <h1>asked questions</h1>
+        <div className="faq-underline"></div>
+      </div>
+
+      <div className="faq-right">
+        <div className="faq-item">
+          <h2>What is your shipping policy?</h2>
+          <p>
+            You ship items within 7 days of receiving your order. Your order comes frozen with dry ice.
+          </p>
+        </div>
+
+        <div className="faq-item">
+          <h2>How can I track my order?</h2>
+          <p>
+            Once your order has been shipped, you will receive a confirmation
+            email with a tracking number and a link to track your shipment.
+          </p>
+        </div>
+
+        <div className="faq-item">
+          <h2>Do you offer international shipping?</h2>
+          <p>
+            No, we do not offer international shipping at this time. Check back later or for
+            more info, email us at{" "}
+            <span className="faq-link">hello@reallygreatsite.com</span>.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Order.js
+++ src/pages/Order.js
@@ -4,11 +4,13 @@ import "../App.css";
 import shin from "../images/shin.jpg";
 import clod from "../images/clod.jpg";
 import neck from "../images/neck.jpg";
+import chuck from "../images/chuck roast.jpg";
+import shortrib from "../images/boneless short rib.jpg"
 
 const sectionData = {
-  1: { name: "Shin", items: [{ id: "shin-1", name: "Beef Shin", price: 7.99, img: shin }] },
+  1:  { name: "Neck", items: [{ id: "neck-1", name: "Beef Neck Bones", price: 6.99, img: chuck},{ id: "neck-2", name: "Boneless Short Rib", price: 6.99, img: neck},{ id: "neck-3", name: "Chuck Roast", price: 16.99, img: shortrib}  ] },
   2: { name: "Clod", items: [{ id: "clod-1", name: "Beef Clod Roast", price: 8.49, img: clod }] },
-  3: { name: "Neck", items: [{ id: "neck-1", name: "Beef Neck Bones", price: 6.99, img: neck }] },
+  3: { name: "Shin", items: [{ id: "shin-1", name: "Beef Shin", price: 7.99, img: shin }] },
 };
 
 export default function Order() {

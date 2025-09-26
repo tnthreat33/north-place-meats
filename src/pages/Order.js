f079148279447368cdf0f96f1466f46137171766
rhamb <rhamb@RACHELYOGA>
2025-09-25 21:37:10 -0400

    add new steak meats

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -5,10 +5,16 @@ import shin from "../images/shin.jpg";
 import clod from "../images/clod.jpg";
 import neck from "../images/neck.jpg";
 import chuck from "../images/chuck roast.jpg";
-import shortrib from "../images/boneless short rib.jpg"
+import shortrib from "../images/boneless short rib.jpg";
+import porterhouse from "../images/porterhouse.jpg";
+import strip from "../images/strip.webp";
+import tbone from "../image/tbone.jpg";
+import tenderloin from "../image/tenderloin.jpg";
 
 const sectionData = {
-  1:  { name: "Neck", items: [{ id: "neck-1", name: "Beef Neck Bones", price: 6.99, img: chuck},{ id: "neck-2", name: "Boneless Short Rib", price: 6.99, img: neck},{ id: "neck-3", name: "Chuck Roast", price: 16.99, img: shortrib}  ] },
+    1:  { name: "Middle Rib", items: [{ id: "mrib-1", name: "Tenderloin Steak", price: 18.00, img: tenderloin},{ id: "mrib-2", name: "Porterhouse Steak", price: 11.00, img: porterhouse},{ id: "mrib-3", name: "T-Bone Steak", price: 10.00, img: tbone} ,{ id: "mrib-4", name: "New York Strip", price: 11.50, img: strip} ] },
+
+  4:  { name: "Neck", items: [{ id: "neck-1", name: "Beef Neck Bones", price: 6.99, img: chuck},{ id: "neck-2", name: "Boneless Short Rib", price: 6.99, img: neck},{ id: "neck-3", name: "Chuck Roast", price: 16.99, img: shortrib}  ] },
   2: { name: "Clod", items: [{ id: "clod-1", name: "Beef Clod Roast", price: 8.49, img: clod }] },
   3: { name: "Shin", items: [{ id: "shin-1", name: "Beef Shin", price: 7.99, img: shin }] },
 };
